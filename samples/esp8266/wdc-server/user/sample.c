935e41b1d269979d27e2929b1a4cf69b153a81aa
ouyang <ouyangchengfeng@xiaomi.com>
2017-09-13 14:37:20 +0800

    add wdc samples

---
+++ samples/esp8266/wdc-server/user/sample.c
@@ -0,0 +1,73 @@
+#include "wdc/Wdc.h"
+#include "esp_common.h"
+
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+ICACHE_FLASH_ATTR
+void tiny_print_mem_info(const char *tag, const char *function)
+{
+    printf("[%s/%s] stack = %d, free heap size: %d\n", tag, function,
+      uxTaskGetStackHighWaterMark(NULL), system_get_free_heap_size());
+}
+
+/******************************************************************************
+ * FunctionName : user_rf_cal_sector_set
+ * Description  : SDK just reversed 4 sectors, used for rf init data and paramters.
+ *                We add this function to force users to set rf cal sector, since
+ *                we don't know which sector is free in user's application.
+ *                sector map for last several sectors : ABCCC
+ *                A : rf cal
+ *                B : rf init data
+ *                C : sdk parameters
+ * Parameters   : none
+ * Returns      : rf cal sector
+ *******************************************************************************/
+uint32 user_rf_cal_sector_set(void)
+{
+    flash_size_map size_map = system_get_flash_size_map();
+    uint32 rf_cal_sec = 0;
+
+    switch (size_map)
+    {
+        case FLASH_SIZE_4M_MAP_256_256:
+            rf_cal_sec = 128 - 5;
+            break;
+
+        case FLASH_SIZE_8M_MAP_512_512:
+            rf_cal_sec = 256 - 5;
+            break;
+
+        case FLASH_SIZE_16M_MAP_512_512:
+        case FLASH_SIZE_16M_MAP_1024_1024:
+            rf_cal_sec = 512 - 5;
+            break;
+
+        case FLASH_SIZE_32M_MAP_512_512:
+        case FLASH_SIZE_32M_MAP_1024_1024:
+            rf_cal_sec = 1024 - 5;
+            break;
+
+        default:
+            rf_cal_sec = 0;
+            break;
+    }
+
+    return rf_cal_sec;
+}
+
+/******************************************************************************
+ * FunctionName : user_init
+ * Description  : entry of user application, init user function here
+ * Parameters   : none
+ * Returns      : none
+ *******************************************************************************/
+void user_init(void)
+{
+    printf("SDK version:%s\n", system_get_sdk_version());
+
+    hap_wdc_init();
+}
---
+++ source/src/Netty/example-wdc/sample_wdc_server.c
@@ -0,0 +1,128 @@
+#include <tiny_log.h>
+#include <bootstrap/Bootstrap.h>
+#include <channel/stream/StreamServerChannel.h>
+#include <channel/stream/StreamServerChannelContext.h>
+#include <channel/SocketChannel.h>
+#include <channel/ChannelIdleStateHandler.h>
+#include <codec-http/HttpMessageCodec.h>
+#include "WdcHandler.h"
+
+#define TAG "HttpServerExample"
+
+static void WdcServerInitializer(Channel *channel, void *ctx)
+{
+    printf("WdcServerInitializer: %s\n", channel->id);
+    SocketChannel_AddLast(channel, ChannelIdleStateHandler(0, 0, 3));
+    SocketChannel_AddLast(channel, HttpMessageCodec());
+    SocketChannel_AddLast(channel, WdcHandler());
+}
+
+static TinyRet init_http_server(Channel *server, uint16_t port)
+{
+    TinyRet ret = TINY_RET_OK;
+
+    StreamServerChannel_Initialize(server, WdcServerInitializer, NULL);
+
+    do
+    {
+        ret = SocketChannel_Open(server, TYPE_TCP_SERVER);
+        if (RET_FAILED(ret))
+        {
+            printf("SocketChannel_Open failed: %d\n", TINY_RET_CODE(ret));
+            break;
+        }
+
+        ret = SocketChannel_Bind(server, port, false);
+        if (RET_FAILED(ret))
+        {
+            printf("SocketChannel_Bind failed: %d\n", TINY_RET_CODE(ret));
+            break;
+        }
+
+        ret = SocketChannel_SetBlock(server, false);
+        if (RET_FAILED(ret))
+        {
+            printf("SocketChannel_SetBlock failed: %d\n", TINY_RET_CODE(ret));
+            break;
+        }
+
+        ret = SocketChannel_Listen(server, ((StreamServerChannelContext *)server->ctx)->maxConnections);
+        if (RET_FAILED(ret))
+        {
+            printf("SocketChannel_Listen failed: %d\n", TINY_RET_CODE(ret));
+            break;
+        }
+    } while (0);
+
+    return ret;
+}
+
+int main()
+{
+    TinyRet ret = TINY_RET_OK;
+    Channel *server1 = NULL;
+//    Channel *server2 = NULL;
+    Bootstrap sb;
+
+    tiny_socket_initialize();
+
+    // new TCP Server
+    server1 = StreamServerChannel_New(6);
+    ret = init_http_server(server1, 9091);
+    if (RET_FAILED(ret))
+    {
+        printf("init_http_server 1 failed: %d\n", TINY_RET_CODE(ret));
+        return 0;
+    }
+
+    // new TCP Server
+//    server2 = StreamServerChannel_New(2);
+//    ret = init_http_server(server1, 9092);
+//    if (RET_FAILED(ret))
+//    {
+//        printf("init_http_server 2 failed: %s\n", tiny_ret_to_str(ret));
+//        return 0;
+//    }
+
+    // Starting Bootstrap
+    ret = Bootstrap_Construct(&sb);
+    if (RET_FAILED(ret))
+    {
+        printf("Bootstrap_Construct failed: %d\n", TINY_RET_CODE(ret));
+        return 0;
+    }
+
+    ret = Bootstrap_AddChannel(&sb, server1);
+    if (RET_FAILED(ret))
+    {
+        printf("Bootstrap_AddChannel failed: %d\n", TINY_RET_CODE(ret));
+        return 0;
+    }
+
+//    ret = Bootstrap_AddChannel(&sb, server2);
+//    if (RET_FAILED(ret))
+//    {
+//        printf("Bootstrap_AddChannel failed: %s\n", tiny_ret_to_str(ret));
+//        return 0;
+//    }
+
+    ret = Bootstrap_Sync(&sb);
+    if (RET_FAILED(ret))
+    {
+        printf("Bootstrap_Sync failed: %d\n", TINY_RET_CODE(ret));
+        return 0;
+    }
+
+    ret = Bootstrap_Shutdown(&sb);
+    if (RET_FAILED(ret))
+    {
+        printf("Bootstrap_Shutdown failed: %d\n", TINY_RET_CODE(ret));
+        return 0;
+    }
+
+    Bootstrap_Dispose(&sb);
+
+    tiny_socket_finalize();
+
+    return 0;
+}
