1aee4dfa845e3ae95cbee0f93625c5a8e1f3668b
ouyang <ouyangchengfeng@xiaomi.com>
2017-09-14 19:38:23 +0800

    update sample: wdc-server

--- samples/esp8266/wdc-server/user/sample.c
+++ samples/esp8266/wdc-server/user/sample.c
@@ -1,4 +1,5 @@
 #include "wdc/Wdc.h"
+#include "wdc/WdcInfo.h"
 #include "esp_common.h"
 
 #include <time.h>
@@ -7,12 +8,86 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+static WdcInfo * wdcInfo = NULL;
+
 ICACHE_FLASH_ATTR
 void tiny_print_mem_info(const char *tag, const char *function)
 {
     printf("[%s/%s] stack = %d, free heap size: %d\n", tag, function,
       uxTaskGetStackHighWaterMark(NULL), system_get_free_heap_size());
 }
+
+/******************************************************************************
+ * FunctionName : wifi_event_handler_cb
+ * Description  : wifi event callback
+ * Parameters   : system event
+ * Returns      : none
+ *******************************************************************************/
+ICACHE_FLASH_ATTR
+static void wifi_event_handler_cb(System_Event_t * event)
+{
+    static int retry = 0;
+
+    if (event == NULL)
+    {
+        return;
+    }
+
+    printf("event: %d\n", event->event_id);
+
+    switch (event->event_id)
+    {
+        case EVENT_STAMODE_DISCONNECTED:
+            if (retry > 6)
+            {
+                hap_wdc_init();
+            }
+            else
+            {
+                retry ++;
+            }
+            break;
+
+        case EVENT_STAMODE_GOT_IP:
+            free(wdcInfo);
+            wdcInfo = NULL;
+            printf("free heap size %d line %d \n", system_get_free_heap_size(), __LINE__);
+            break;
+
+        default:
+            break;
+    }
+
+    return;
+}
+
+/******************************************************************************
+ * FunctionName : wifi_config
+ * Description  : wifi_config task
+ * Parameters   : task param
+ * Returns      : none
+ *******************************************************************************/
+ICACHE_FLASH_ATTR
+static void wifi_config(void *pvParameters)
+{
+    struct ip_info ip_config;
+    struct station_config sta_config;
+
+    memset(&sta_config, 0, sizeof(struct station_config));
+    wifi_set_opmode(STATION_MODE);
+    memcpy(sta_config.ssid, wdcInfo->ssid, strlen(wdcInfo->ssid));
+    memcpy(sta_config.password, wdcInfo->password, strlen(wdcInfo->password));
+    wifi_station_set_config(&sta_config);
+
+    wifi_station_disconnect();
+    wifi_station_connect();
+
+    while (1)
+    {
+        vTaskDelay(10);
+    }
+}
+
 
 /******************************************************************************
  * FunctionName : user_rf_cal_sector_set
@@ -69,5 +144,27 @@ void user_init(void)
 {
     printf("SDK version:%s\n", system_get_sdk_version());
 
+    /**
+     * 读取参数
+     *   1. 如果有AP信息, 开始连接AP. 如果连接失败, 则进入AP模式.
+     *   2. 如果没有AP, 进入AP模式. 如果收到配置请求, 写入AP信息后重启.
+     */
+#if 1
+    wdcInfo = (WdcInfo *)malloc(sizeof(WdcInfo));
+
+    if (WdcInfo_Read(wdcInfo))
+    {
+        printf("ssid: %s\n", wdcInfo->ssid);
+        printf("password: %s\n", wdcInfo->password);
+
+        wifi_set_event_handler_cb(wifi_event_handler_cb);
+        xTaskCreate(wifi_config, "wifi", 256, NULL, 4, NULL);
+    }
+    else
+    {
+        hap_wdc_init();
+    }
+#else
     hap_wdc_init();
+#endif
 }
